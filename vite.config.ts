4d1bfd70f9d31ea3192b372fe84ac585b332879c
yooss2006 <68742190+yooss2006@users.noreply.github.com>
2025-01-14 23:03:50 +0900

    Feat: ESLint, Prettier, TypeScript 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,21 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import path from 'path';
 
-import path from "path";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteTsconfigPaths()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
+  },
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 3000,
   },
 });
